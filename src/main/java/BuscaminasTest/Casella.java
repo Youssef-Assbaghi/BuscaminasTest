368b7d719d98dba93ec7114b09a86655b1ae67d2
Youssef-Assbaghi <youssef.assbaghi@e-campus.uab.cat>
2020-10-16 17:23:37 +0200

    Primer test para cambiar Bandera

--- src/main/java/BuscaminasTest/Casella.java
+++ src/main/java/BuscaminasTest/Casella.java
@@ -1,5 +1,13 @@
 package BuscaminasTest;
 
 public class Casella {
-
+	private int minasCercanas;
+	private boolean esMina;
+	private boolean estado;
+	private boolean bandera;
+	
+	public boolean getBandera() {return this.bandera;}
+	public void cambiarBandera() {
+		
+	}
 }
--- src/test/java/BuscaminasTest/CasellaTest.java
+++ src/test/java/BuscaminasTest/CasellaTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class CasellaTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCambiarBandera() {
+		Casella bandera =new Casella();
+		bandera.cambiarBandera();
+		
+		assertFalse("Como se inicializa a zero debe devolver un True conforme hay Bandera", bandera.getBandera());
+		
+		bandera.cambiarBandera();
+		assertTrue("Una vez cambia de bandera al volver a darle se debe quitar y devolver False", bandera.getBandera());
 	}
 
 }
