a6f7bca105162ba78085e6691e3caf04f84bb131
Gerard <gerard.noguer@gmail.com>
2020-11-04 12:55:42 +0100

    Formateo y test añadidos test a ponerBander

--- src/main/java/BuscaminasTest/Tablero.java
+++ src/main/java/BuscaminasTest/Tablero.java
@@ -1,101 +1,101 @@
 package BuscaminasTest;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Random;
 
 public final class Tablero {
-	private final int arr[] = {10,16,24};
-	private final int nMinas[] = {10,40,99};
-	
+	private final int arr[] = { 10, 16, 24 };
+	private final int nMinas[] = { 10, 40, 99 };
+
 	private int numMinas;
 	private int dificultad;
 	private int nFilas;
 	private int nColumnas;
 	private Casella[][] casillas; // Debe ser privada y acceder mediante getters y setters
-	
-	
+
 	public Tablero(int dificultad) {
-		//creamos random aqui
-		//interfaz rndom en una clase
-		//y cuando la llamemmos la vamos llamando poco a poco
-		if(dificultad<=0)
-			dificultad=0;
-		else if(dificultad>2)
+		// creamos random aqui
+		// interfaz rndom en una clase
+		// y cuando la llamemmos la vamos llamando poco a poco
+		if (dificultad <= 0)
+			dificultad = 0;
+		else if (dificultad > 2)
 			dificultad = 0;
 		this.setDificultad(dificultad);
-		nFilas=arr[dificultad];
-		nColumnas=arr[dificultad];
+		nFilas = arr[dificultad];
+		nColumnas = arr[dificultad];
 		setNumMinas(nMinas[dificultad]);
 
 		generarTablero();
 
 	}
-	
+
 	public Casella getCasillas(int fila, int columna) {
 		return casillas[fila][columna];
 	}
-	
+
 	public void generarTablero() {
-		casillas=new Casella[nFilas][nColumnas];
-		for (int j=0;j<=nFilas-1;j++)
-			for (int i = 0; i <= nColumnas-1; i++) 
-				casillas[j][i]=new Casella();
-				
+		casillas = new Casella[nFilas][nColumnas];
+		for (int j = 0; j <= nFilas - 1; j++)
+			for (int i = 0; i <= nColumnas - 1; i++)
+				casillas[j][i] = new Casella();
+
 	}
-		
-	public void pintarTablero() {}
+
+	public void pintarTablero() {
+	}
 
 	public void ponerMinas() {
-		Random rand=new Random();
-	    int mineCount = 0;
-	    while (mineCount < numMinas)
-	    {
-	        int filaRandom =  (int) (rand.nextDouble() * nFilas-1);
-	        int colRandom =  (int) (rand.nextDouble() * nColumnas-1);
-	        if (casillas[filaRandom][colRandom].esMina())
-	            continue;
-	        else if(!casillas[filaRandom][colRandom].getAbierta()) // Ver hacer un mock para testearlo
-	        {
-	            casillas[filaRandom][colRandom].setEsMina(true);
-	            mineCount++;
-	        }
-	    }
+		Random rand = new Random();
+		int mineCount = 0;
+		while (mineCount < numMinas) {
+			int filaRandom = (int) (rand.nextDouble() * nFilas - 1);
+			int colRandom = (int) (rand.nextDouble() * nColumnas - 1);
+			if (casillas[filaRandom][colRandom].esMina())
+				continue;
+			else if (!casillas[filaRandom][colRandom].getAbierta()) // Ver hacer un mock para testearlo
+			{
+				casillas[filaRandom][colRandom].setEsMina(true);
+				mineCount++;
+			}
+		}
 
-		
 	}
-	
+
 	public int marcarCasilla(int fila, int columna) {
-		if(fila<0 || fila >= nFilas) 
+		if (fila < 0 || fila >= nFilas)
 			return -1;
-		
-		if(columna < 0 || columna >= nColumnas)
+
+		if (columna < 0 || columna >= nColumnas)
 			return -1;
-		
-		if(casillas[fila][columna].getAbierta()) {
+
+		if (casillas[fila][columna].getAbierta()) {
 			return 0;
-		}else {
+		} else {
 			casillas[fila][columna].abrirCasilla();
 			return 0;
 		}
-			
+
 	}
-	
+
 	public int ponerBandera(int fila, int columna) {
-		if(fila<0 || fila >= nFilas) 
+		if (fila < 0 || fila >= nFilas)
 			return -1;
-		
-		if(columna < 0 || columna >= nColumnas)
+
+		if (columna < 0 || columna >= nColumnas)
 			return -1;
-		
+
 		casillas[fila][columna].cambiarBandera();
 		return 0;
 
 	}
-	
-	public int getNFilas() {return nFilas-1; }
-	
-	public int getNColumnas() {return nColumnas-1; }
+
+	public int getNFilas() {
+		return nFilas - 1;
+	}
+
+	public int getNColumnas() {
+		return nColumnas - 1;
+	}
 
 	public int getNumMinas() {
 		return numMinas;
@@ -113,58 +113,45 @@ public final class Tablero {
 		this.dificultad = dificultad;
 	}
 
-	public void numMinasAlrededor(int fila, int col) {
+	public void sumarMinasAlrededor(int fila, int col) { // A ESTA FILA Y A ESTA COLUMNA LE PONEMOS UNA MINA
+// SUMAMOS 1 A TODAS LAS DE ALREDEDOR
+		if (fila - 1 >= 0 && col + 1 >= 0)
+			casillas[fila - 1][col + 1].sumarMinaCercana();
 
-		if(fila - 1 >= 0 && col + 1 >= 0) {
-			casillas[fila-1][col+1].sumarMinaCercana();
-		}else if(fila - 1 >= 0 && col - 1 >= 0) {
-			casillas[fila-1][col-1].sumarMinaCercana();
-		}else if(fila + 1 < nFilas-1 && col + 1 < nColumnas-1) {
-			casillas[fila+1][col+1].sumarMinaCercana();
-		}else if(fila + 1 < nFilas-1 && col - 1 >= 0) {
-			casillas[fila+1][col-1].sumarMinaCercana();
-		}else if(fila - 1 >= 0) {  
-			casillas[fila-1][col].sumarMinaCercana();
-			
-		}else if(fila + 1 >= nFilas-1) {
-			casillas[fila+1][col].sumarMinaCercana();
-			
-		}else if(col + 1 < nColumnas-1) {
+		if (fila - 1 >= 0 && col - 1 >= 0)
+			casillas[fila - 1][col - 1].sumarMinaCercana();
+
+		if (fila + 1 < nFilas - 1 && col + 1 < nColumnas - 1)
+			casillas[fila + 1][col + 1].sumarMinaCercana();
+
+		if (fila + 1 < nFilas - 1 && col - 1 >= 0)
+			casillas[fila + 1][col - 1].sumarMinaCercana();
+
+		if (fila - 1 >= 0)
+			casillas[fila - 1][col].sumarMinaCercana();
+
+		if (fila + 1 <= nFilas - 1)
+			casillas[fila + 1][col].sumarMinaCercana();
+
+		if (col + 1 < nColumnas - 1)
 			casillas[fila][col + 1].sumarMinaCercana();
-			
-		}else if(col - 1 >= 0) {
-			casillas[fila][col-1].sumarMinaCercana();
-			
-		}
-		
-		
-	}
-}
 
+		if (col - 1 >= 0)
+			casillas[fila][col - 1].sumarMinaCercana();
+	}
+
+}
 
 /**
- * Al haberse creado las minas el jugador selecciona una casilla para abrir, la casilla
-seleccionada y las casillas cercanas se igualan a zero forma que si habia minas estas se
-borran.
-Despues de haber borrado las minas cercanas a la casilla seleccionada se checa cada casilla
-para verificar si tiene una bomba cerca, si tiene una bomba cerca se le suma 1 a menos de
-que sea bomba o este fuera del rango de la matriz.
-La casilla que selecciono el jugador se abre reemplazando la casilla de la matriz que el
-jugador ve con la casilla que esta en el mismo lugar de la matriz que contiene toda la
-informacion de las minas junto con las 8 casillas vecinas, si alguna de las casillas que se
-abrio es cero todas las casillas cercanas a estas tambien se abren hasta que todas las casillas
-cercanas a un ya abierto 0 esten tambien abiertas.*/
-
-
-/*private void ClickSquare(int x, int y)
-{
-   // Did the user click an already exposed square?  If so, ignore
-   if (matriz[x][y].getDiscovered()) return;
-   matriz[x][y].SetDiscovered(true);
-   if (matriz[x][y].getNumberOfMinesAround != 0) return;
-   // If empty, click all the neighbors
-   for (int xloop = x - 1; xloop <= x + 1; xloop++)
-       for (int yloop = y - 1; yloop <= y + 1; yloop++)
-           ClickSquare(xloop, yloop);
-}
-*/
+ * Al haberse creado las minas el jugador selecciona una casilla para abrir, la
+ * casilla seleccionada y las casillas cercanas se igualan a zero forma que si
+ * habia minas estas se borran. Despues de haber borrado las minas cercanas a la
+ * casilla seleccionada se checa cada casilla para verificar si tiene una bomba
+ * cerca, si tiene una bomba cerca se le suma 1 a menos de que sea bomba o este
+ * fuera del rango de la matriz. La casilla que selecciono el jugador se abre
+ * reemplazando la casilla de la matriz que el jugador ve con la casilla que
+ * esta en el mismo lugar de la matriz que contiene toda la informacion de las
+ * minas junto con las 8 casillas vecinas, si alguna de las casillas que se
+ * abrio es cero todas las casillas cercanas a estas tambien se abren hasta que
+ * todas las casillas cercanas a un ya abierto 0 esten tambien abiertas.
+ */
--- src/test/java/BuscaminasTest/TableroTest.java
+++ src/test/java/BuscaminasTest/TableroTest.java
@@ -1,6 +1,8 @@
-	package BuscaminasTest;
+package BuscaminasTest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,232 +10,213 @@ import org.junit.Test;
 public class TableroTest {
 
 	private Tablero tablero;
-	
-	@Before 
+
+	@Before
 	public void setUp() {
-         tablero = new Tablero(0);
-    }
-	
-	
+		tablero = new Tablero(0);
+	}
+
 	@Test
 	public void testGenerarTablero() {
-		
+
 		int checksum = 0;
-		for(int i = 0; i <= tablero.getNFilas(); i++) {
-			for(int j = 0; j <= tablero.getNColumnas(); j++) {
-				
-				assertFalse(tablero.getCasillas(i,j).getAbierta());
-				assertFalse(tablero.getCasillas(i,j).getBandera());
+		for (int i = 0; i <= tablero.getNFilas(); i++) {
+			for (int j = 0; j <= tablero.getNColumnas(); j++) {
+
+				assertFalse(tablero.getCasillas(i, j).getAbierta());
+				assertFalse(tablero.getCasillas(i, j).getBandera());
 				checksum++;
 			}
 		}
 		assertEquals(100, checksum);
-		
-		//accedemos a posiciones arroneas o extremos o negativas
-		
-		tablero=new Tablero(3);
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNFilas());    
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNColumnas());    
-		
-		tablero=new Tablero(-5);
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNFilas());    
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNColumnas());    
-		
-		tablero=new Tablero(8);
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNFilas());    
-		assertEquals("En caso de no cumplir parametros se definira com 10,10",9,tablero.getNColumnas());    
-		
+
+		// accedemos a posiciones arroneas o extremos o negativas
+
+		tablero = new Tablero(3);
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNFilas());
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNColumnas());
+
+		tablero = new Tablero(-5);
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNFilas());
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNColumnas());
+
+		tablero = new Tablero(8);
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNFilas());
+		assertEquals("En caso de no cumplir parametros se definira com 10,10", 9, tablero.getNColumnas());
+
 	}
-	
+
 	@Test
-	public void testPonerMinas(){
-		
+	public void testPonerMinas() {
+
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,10,tablero.getNumMinas());
+		assertEquals("Dificultad 0= 10 minas", 10, tablero.getNumMinas());
 		tablero = new Tablero(1);
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,40,tablero.getNumMinas());
-		
+		assertEquals("Dificultad 0= 10 minas", 40, tablero.getNumMinas());
+
 		tablero = new Tablero(2);
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,99,tablero.getNumMinas());
-		
-		
+		assertEquals("Dificultad 0= 10 minas", 99, tablero.getNumMinas());
+
 		tablero = new Tablero(-1);
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,10,tablero.getNumMinas());
-		
+		assertEquals("Dificultad 0= 10 minas", 10, tablero.getNumMinas());
+
 		tablero = new Tablero(3);
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,10,tablero.getNumMinas());
-		
+		assertEquals("Dificultad 0= 10 minas", 10, tablero.getNumMinas());
+
 		tablero = new Tablero(81);
 		tablero.ponerMinas();
-		assertEquals("Dificultad 0= 10 minas" ,10,tablero.getNumMinas());
-		
+		assertEquals("Dificultad 0= 10 minas", 10, tablero.getNumMinas());
+
 	}
-	
+
 	@Test
 	public void testPintarTablero() {
-		
+
 		tablero.pintarTablero();
-		
+
 	}
-	
+
 	@Test
 	public void testMarcarCasilla() {
 		int k;
-		for(int i=-1;i<4;i++){
-			MockRandom random=new MockRandom(0.0);
-			tablero=new Tablero(i);
+		for (int i = -1; i < 4; i++) {
+			tablero = new Tablero(i);
 			tablero.ponerMinas();
-			k=tablero.marcarCasilla(0,0); // static
-			assertTrue("La casilla se abre",tablero.getCasillas(0,0).getAbierta());
-			assertEquals(0,k);
-			k=tablero.marcarCasilla(0,0); //static
-			assertTrue("La casilla no se puede cerrar",tablero.getCasillas(0,0).getAbierta());
-			assertEquals(0,k);
-			k=tablero.marcarCasilla(0,tablero.getNColumnas());
-			assertEquals(0,k);
-			assertTrue("La casilla se abre",tablero.getCasillas(0,tablero.getNColumnas()).getAbierta());
-			k=tablero.marcarCasilla(0,tablero.getNColumnas()+1);
-			assertEquals(-1,k);
-			k=tablero.marcarCasilla(tablero.getNFilas(),0);
-			assertEquals(0,k);
-			assertTrue("La casilla se abre",tablero.getCasillas(0,tablero.getNColumnas()).getAbierta());
-			k=tablero.marcarCasilla(tablero.getNFilas(),tablero.getNColumnas()+1);	
-			assertEquals(-1,k);
-			k=tablero.marcarCasilla(tablero.getNFilas(),-8);	
-			assertEquals(-1,k);
-			k=tablero.marcarCasilla(tablero.getNFilas(),tablero.getNColumnas());
-			assertEquals(0,k);
-			assertTrue("La casilla se abre",tablero.getCasillas(0,tablero.getNColumnas()).getAbierta());
-			k=tablero.marcarCasilla(tablero.getNFilas()+1,tablero.getNColumnas()+1);
-			assertEquals(-1,k);
-			k=tablero.marcarCasilla(-1,-1); //static
-			assertEquals(-1,k);
-			k=tablero.marcarCasilla(100,-100); //static
-			assertEquals(-1,k);
+			k = tablero.marcarCasilla(0, 0); // static
+			assertTrue("La casilla se abre", tablero.getCasillas(0, 0).getAbierta());
+			assertEquals(0, k);
+			k = tablero.marcarCasilla(0, 0); // static
+			assertTrue("La casilla no se puede cerrar", tablero.getCasillas(0, 0).getAbierta());
+			assertEquals(0, k);
+			k = tablero.marcarCasilla(0, tablero.getNColumnas());
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getAbierta());
+			k = tablero.marcarCasilla(0, tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.marcarCasilla(tablero.getNFilas(), 0);
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getAbierta());
+			k = tablero.marcarCasilla(tablero.getNFilas(), tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.marcarCasilla(tablero.getNFilas(), -8);
+			assertEquals(-1, k);
+			k = tablero.marcarCasilla(tablero.getNFilas(), tablero.getNColumnas());
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getAbierta());
+			k = tablero.marcarCasilla(tablero.getNFilas() + 1, tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.marcarCasilla(-1, -1); // static
+			assertEquals(-1, k);
+			k = tablero.marcarCasilla(100, -100); // static
+			assertEquals(-1, k);
 		}
-		
 
 	}
-	/**
-            N.W   N   N.E 
-              \   |   / 
-               \  |  / 
-            W----Cell----E 
-                /  | \ 
-               /   |  \ 
-            S.W    S   S.E 
-  
-        Cell-->Current Cell (row, col) 
-        N -->  North        (row-1, col) 
-        S -->  South        (row+1, col) 
-        E -->  East         (row, col+1) 
-        W -->  West            (row, col-1) 
-        N.E--> North-East   (row-1, col+1) 
-        N.W--> North-West   (row-1, col-1) 
-        S.E--> South-East   (row+1, col+1) 
-        S.W--> South-West   (row+1, col-1)
-	 */
+
 	@Test
 	public void testNumMinasAlrededor() {
-		
+
 		int fila = 5;
 		int col = 5;
-		
-		//0 minas
-		
-		tablero.numMinasAlrededor(fila-1,col);
-		assertEquals(tablero.getCasillas(fila-1,col).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila+1,col);
-		assertEquals(tablero.getCasillas(fila+1, col).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila,col+1);
-		assertEquals(tablero.getCasillas(fila, col+1).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila,col-1);
-		assertEquals(tablero.getCasillas(fila,col-1).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila-1,col+1);
-		assertEquals(tablero.getCasillas(fila-1,col+1).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila-1,col-1);
-		assertEquals(tablero.getCasillas(fila-1,col+1).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila+1,col+1);
-		assertEquals(tablero.getCasillas(fila,col).getminasCercanas(), 0);
-		
-		tablero.numMinasAlrededor(fila+1,col-1);
-		assertEquals(tablero.getCasillas(fila,col).getminasCercanas(), 0);
-		
-		//1 mina
-		
-		tablero.getCasillas(fila,col).setEsMina(true);
-		
-		tablero.numMinasAlrededor(fila-1,col);
-		assertEquals(tablero.getCasillas(fila-1,col).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila+1,col);
-		assertEquals(tablero.getCasillas(fila+1, col).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila,col+1);
-		assertEquals(tablero.getCasillas(fila, col+1).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila,col-1);
-		assertEquals(tablero.getCasillas(fila,col-1).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila-1,col+1);
-		assertEquals(tablero.getCasillas(fila-1,col+1).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila-1,col-1);
-		assertEquals(tablero.getCasillas(fila-1,col+1).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila+1,col+1);
-		assertEquals(tablero.getCasillas(fila,col).getminasCercanas(), 1);
-		
-		tablero.numMinasAlrededor(fila+1,col-1);
-		assertEquals(tablero.getCasillas(fila,col).getminasCercanas(), 1);
-		
-		//8 minas
+
+		// 0 minas
+
+		assertEquals(tablero.getCasillas(fila - 1, col).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila + 1, col).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila, col + 1).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila, col - 1).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila - 1, col + 1).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila - 1, col - 1).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila + 1, col + 1).getminasCercanas(), 0);
+		assertEquals(tablero.getCasillas(fila + 1, col - 1).getminasCercanas(), 0);
+
+		// 1 minas
+
+		tablero.getCasillas(fila, col).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila, col);
+
+		assertEquals(tablero.getCasillas(fila - 1, col).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila, col + 1).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila, col - 1).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila - 1, col + 1).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila - 1, col - 1).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila + 1, col + 1).getminasCercanas(), 1);
+		assertEquals(tablero.getCasillas(fila + 1, col - 1).getminasCercanas(), 1);
+
+		// 8 minas
 		tablero = new Tablero(0);
-		
-		tablero.getCasillas(fila-1,col).setEsMina(true);
-		tablero.getCasillas(fila+1,col).setEsMina(true);
-		tablero.getCasillas(fila,col+1).setEsMina(true);
-		tablero.getCasillas(fila,col-1).setEsMina(true);
-		tablero.getCasillas(fila-1,col+1).setEsMina(true);
-		tablero.getCasillas(fila-1,col-1).setEsMina(true);
-		tablero.getCasillas(fila+1,col+1).setEsMina(true);
-		tablero.getCasillas(fila+1,col-1).setEsMina(true);
-		
-		tablero.numMinasAlrededor(fila,col);
-		assertEquals(tablero.getCasillas(fila,col).getminasCercanas(),8);
-		
-		
-		
-		}
-		
-	
+
+		tablero.getCasillas(fila - 1, col).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila - 1, col);
+		tablero.getCasillas(fila + 1, col).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila + 1, col);
+		tablero.getCasillas(fila, col + 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila, col + 1);
+		tablero.getCasillas(fila, col - 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila, col - 1);
+
+		tablero.getCasillas(fila - 1, col + 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila - 1, col + 1);
+		tablero.getCasillas(fila - 1, col - 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila - 1, col - 1);
+		tablero.getCasillas(fila + 1, col + 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila + 1, col + 1);
+		tablero.getCasillas(fila + 1, col - 1).setEsMina(true);
+		tablero.sumarMinasAlrededor(fila + 1, col - 1);
+
+		assertEquals(tablero.getCasillas(fila, col).getminasCercanas(), 8);
+
+	}
+
 	@Test
 	public void testPonerBandera() {
-		
-		//Al empezar no puede haber una bandera en ninguna casilla.
-		//Tampoco puedes poner banderas al principio del juego
-		
+
 		assertFalse(tablero.getCasillas(1, 1).getBandera());
-		
 		tablero.getCasillas(1, 1).cambiarBandera();
-		
 		assertTrue(tablero.getCasillas(1, 1).getBandera());
-		
-		//No se puede poner una bandera en una casilla abierta
-		
-		tablero.getCasillas(1,2).abrirCasilla();
-		tablero.getCasillas(1,2).cambiarBandera();
-		assertFalse(tablero.getCasillas(1,2).getBandera());
+
+		// No se puede poner una bandera en una casilla abierta
+
+		tablero.getCasillas(1, 2).abrirCasilla();
+		tablero.getCasillas(1, 2).cambiarBandera();
+		assertFalse(tablero.getCasillas(1, 2).getBandera());
+
+		int k;
+		for (int i = -1; i < 4; i++) {
+			tablero = new Tablero(i);
+			k = tablero.ponerBandera(0, 0);
+
+			assertTrue("La casilla se abre", tablero.getCasillas(0, 0).getBandera());
+			assertEquals(0, k);
+			k = tablero.ponerBandera(0, 0);
+			assertFalse("La casilla no se puede cerrar", tablero.getCasillas(0, 0).getBandera());
+			assertEquals(0, k);
+			k = tablero.ponerBandera(0, tablero.getNColumnas());
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getBandera());
+			k = tablero.ponerBandera(0, tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.ponerBandera(tablero.getNFilas(), 0);
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getBandera());
+			k = tablero.ponerBandera(tablero.getNFilas(), tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.ponerBandera(tablero.getNFilas(), -8);
+			assertEquals(-1, k);
+			k = tablero.ponerBandera(tablero.getNFilas(), tablero.getNColumnas());
+			assertEquals(0, k);
+			assertTrue("La casilla se abre", tablero.getCasillas(0, tablero.getNColumnas()).getBandera());
+			k = tablero.ponerBandera(tablero.getNFilas() + 1, tablero.getNColumnas() + 1);
+			assertEquals(-1, k);
+			k = tablero.ponerBandera(-1, -1); // static
+			assertEquals(-1, k);
+			k = tablero.ponerBandera(100, -100); // static
+			assertEquals(-1, k);
+		}
 
 	}
 
