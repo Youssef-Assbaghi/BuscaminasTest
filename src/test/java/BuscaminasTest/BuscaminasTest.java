6d03559deb1cddfbbd9d391505591e4969023a78
Gerard <gerard.noguer@gmail.com>
2020-11-16 14:51:40 +0100

    Test para una nueva función initVista

--- src/test/java/BuscaminasTest/BuscaminasTest.java
+++ src/test/java/BuscaminasTest/BuscaminasTest.java
@@ -2,6 +2,7 @@ package BuscaminasTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -26,8 +27,22 @@ public class BuscaminasTest {
 		assertEquals("Debe ser zero", 0, testeable.getDificultad());
 		assertTrue("Esta casilla se abre", testeable.getCasillas(0, 0).getAbierta());
 
-		// juego=new Buscaminas(null);
-		// Buscaminas.main(hey);
+//		juego = new Buscaminas(null);
+//		Buscaminas.main(hey);
+
+	}
+
+	@Test
+	public void testInitVista() {
+		MockView prueba = new MockView();
+		Buscaminas juego = new Buscaminas(prueba);
+		String[] hey = null;
+		Buscaminas.main(hey);
+
+		juego = new Buscaminas(null);
+		juego.initVista();
+
+		assertNotNull(juego.getVista());
 
 	}
 
@@ -63,30 +78,37 @@ public class BuscaminasTest {
 
 	@Test
 	public void detectarDerrota() {
+
 		MockView prueba = new MockView();
+		ValorRandom r = new ValorRandom();
 		Buscaminas juego = new Buscaminas(prueba);
-		String[] hey = null;
-		Buscaminas.main(hey);
-		Buscaminas.detectarDerrota();
+		Tablero tablero = juego.getTablero();
+		juego.detectarDerrota(0, 0);
+
 		// Expected false
 		assertFalse(juego.getSalir());
 
 		// Expect perder partida
-		ValorRandom r = new ValorRandom();
+		r = new ValorRandom();
 		juego = new Buscaminas(prueba);
-		Tablero tablero = juego.getTablero();
+		tablero = juego.getTablero();
 		tablero.ponerMinas(r);
 		Casella casillas[][] = tablero.getMatrizCasillas();
 
-		for (int i = 0; i < casillas.length; i++) {
-			for (int j = 0; j < casillas[i].length; j++) {
+		int i = 0;
+		int j = 0;
+
+		tablero.marcarCasilla(0, 0);
+		outerloop: for (i = 0; i < casillas.length; i++) {
+			for (j = 0; j < casillas[i].length; j++) {
 				if (casillas[i][j].esMina()) {
 					tablero.marcarCasilla(i, j);
+					break outerloop;
 				}
 			}
 		}
 
-		Buscaminas.detectarDerrota();
-		assertTrue(juego.getVictoria());
+		Buscaminas.detectarDerrota(i, j);
+		assertTrue(juego.getSalir());
 	}
 }
